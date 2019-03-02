9fc505e10fcc0d42b51274aa699b1d20f16dbe3a
travonf <travonf@qq.com>
2019-03-03 00:37:40 +0800

    03. 管理资源 回退处理

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import _ from 'lodash';
-import './index.css';
-import Icon from './project.svg';
-import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // 将图像添加到我们现有的 div
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    console.log(Data);
 
     return element;
 }
